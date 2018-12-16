ce20e9e97710f867af6ee0919736eaa1e3c1fe2e
Seitmagambet Olzhas <sei7magambet@gmail.com>
2018-12-16 13:10:55 +0600

    merge function

--- mergeSort.js
+++ mergeSort.js
@@ -7,16 +7,16 @@ function merge(arr1, arr2) {
   let len1 = arr1.length;
   let len2 = arr2.length;
 
-  while (i < len1 || j < len2) {
-    if (arr1[i] < arr2[j])
-      value = arr1[i++];
-    else
-      value = arr2[j++];
-
+  while (i < len1 && j < len2) {
+    if (arr1[i] < arr2[j]) value = arr1[i++];
+    else value = arr2[j++];
     value !== undefined && res.push(value);
   }
+
+  while (i < len1) res.push(arr1[i++]);
+  while (j < len2) res.push(arr2[j++]);
 
   return res;
 }
 
-console.log(merge([1, 10, 50], [2, 14, 99, 100]));
+console.log(merge([1, 2, 14, 99], [50]));
