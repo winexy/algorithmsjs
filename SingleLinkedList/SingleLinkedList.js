2ded4b3f45b581f788f8816544f47e1c137cf3eb
Seitmagambet Olzhas <sei7magambet@gmail.com>
2018-12-21 22:50:25 +0600

    single linked list pop

--- SingleLinkedList/SingleLinkedList.js
+++ SingleLinkedList/SingleLinkedList.js
@@ -22,6 +22,38 @@ class SingleLinkedList {
   }
 
 
+  pop() {
+    if (!this.head) return;
+
+    let current = this.head;
+    let prev = current
+    
+    while (current.next) {
+      prev = current;
+      current = current.next
+    }
+    
+    this.tail = prev;
+    this.tail.next = null;
+    this.length--;
+    console.log('len', this.length);
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    
+    return current.val;
+  }
+
+
+  toArray() {
+    return [...this];
+  }
+
+
+
+
+
   [Symbol.iterator]() {
     return {
       next: () => {
@@ -31,7 +63,9 @@ class SingleLinkedList {
         if (!done) {
           value = this.current.val;
           this.current = this.current.next;
-        } 
+        } else {
+          this.current = this.head;
+        }
         return { done, value };
       }
     }
--- SingleLinkedList/index.js
+++ SingleLinkedList/index.js
@@ -3,11 +3,12 @@ const SingleLinkedList = require('./SingleLinkedList');
 const list = new SingleLinkedList();
 
 list.push(1);
-list.push(2);
-list.push(3);
+// list.push(2);
+// list.push(3);
+
+// console.log(list.toArray());
 
 
-console.log(...list);
-// for (let val of list.iterator()) {
-  // console.log('a ' + val);
-// }
+// console.log(list.toArray());
+
+
